2e3381e25a7896c957fce1d52faa6884049535c9
aungzawphyo <aungzawphyo1102@gmail.com>
2024-10-16 22:40:15 +0630

    fix page route list icon

--- components/layouts/PageList.tsx
+++ components/layouts/PageList.tsx
@@ -6,23 +6,28 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
+import Dashboard from '@mui/icons-material/Dashboard';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import GroupIcon from '@mui/icons-material/Group';
+import SellIcon from '@mui/icons-material/Sell';
+import CategoryIcon from '@mui/icons-material/Category';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import SegmentIcon from '@mui/icons-material/Segment';
 
 interface PageListProps {
     onToggle?: (newOpen: boolean) => () => void;
 }
 
 const routes = [
-    { name: 'Dashboard', uri: '/' },
-    { name: 'Products', uri: '/products' },
-    { name: 'Categories', uri: '/categories' },
-    { name: 'Variants', uri: '/variants' },
-    { name: 'Orders', uri: '/orders' },
-    { name: 'Users', uri: '/users' },
-    { name: 'Admins', uri: '/admins' },
+    { name: 'Dashboard', uri: '/', icon: <Dashboard className='text-white' /> },
+    { name: 'Products', uri: '/products', icon: <FormatListBulletedIcon className='text-white' /> },
+    { name: 'Categories', uri: '/categories', icon: <CategoryIcon className='text-white' /> },
+    { name: 'Variants', uri: '/variants', icon: <SegmentIcon className='text-white' /> },
+    { name: 'Orders', uri: '/orders', icon: <SellIcon className='text-white' /> },
+    { name: 'Users', uri: '/users', icon: <GroupIcon className='text-white' /> },
+    { name: 'Admins', uri: '/admins', icon: <AdminPanelSettingsIcon className='text-white' /> },
 ]
 
 const PageList: React.FC<PageListProps> = ({ onToggle = null }) => {
@@ -42,7 +47,8 @@ const PageList: React.FC<PageListProps> = ({ onToggle = null }) => {
                         <ListItem disablePadding onClick={handleToggle.bind(null, false)}>
                             <ListItemButton>
                                 <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon className='text-white' /> : <MailIcon className='text-white' />}
+                                    {/* <MailIcon className='text-white' /> */}
+                                    {route.icon}
                                 </ListItemIcon>
                                 <ListItemText primary={route.name} />
                             </ListItemButton>
@@ -51,7 +57,7 @@ const PageList: React.FC<PageListProps> = ({ onToggle = null }) => {
                 ))}
             </List>
             <Divider />
-            <List>
+            {/* <List>
                 {['All mail', 'Trash', 'Spam'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
@@ -62,7 +68,7 @@ const PageList: React.FC<PageListProps> = ({ onToggle = null }) => {
                         </ListItemButton>
                     </ListItem>
                 ))}
-            </List>
+            </List> */}
         </Box>
     )
 }
